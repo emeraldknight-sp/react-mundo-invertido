b5d27201459f20e9d0cc5c66eca4ba5c66db8ccc
David <contato.davidalmeida@outlook.com>
2023-12-21 09:27:27 -0300

    🤡 Mock things

---
+++ src/mock/gallery.ts
@@ -0,0 +1,27 @@
+import VecnaImage from "../assets/images/serie-image-01.webp";
+import PersonagensImage from "../assets/images/serie-image-02.webp";
+import MainImage from "../assets/images/serie-image-03.webp";
+
+interface ImageProps {
+  id: number;
+  src: string;
+  alt: string;
+}
+
+export const gallery: ImageProps[] = [
+  {
+    id: 1,
+    src: VecnaImage,
+    alt: "Vecna",
+  },
+  {
+    id: 2,
+    src: PersonagensImage,
+    alt: "Characters",
+  },
+  {
+    id: 3,
+    src: MainImage,
+    alt: "Poster",
+  },
+];
