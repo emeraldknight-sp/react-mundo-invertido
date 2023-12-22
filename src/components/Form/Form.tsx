977e13b3dd4963a5699c670a3a95230c8af9c009
David <contato.davidalmeida@outlook.com>
2023-12-22 14:41:33 -0300

    ✅ Update: Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import { FormProps } from "../../@types";
+import { FormContext } from "../../context/FormContext";
 import styles from "./Form.module.scss";
 
-export const Form = ({ children }: { children: React.ReactNode }) => {
+export const Form = ({ children }: FormProps) => {
+	const { sendFormData } = useContext(FormContext);
+
 	return (
 		<div className={styles.container}>
-			<form className={styles.form} action="">
+			<form className={styles.form} onSubmit={(e) => sendFormData(e)}>
 				{children}
 			</form>
 		</div>
