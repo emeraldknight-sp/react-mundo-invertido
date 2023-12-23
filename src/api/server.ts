cde188d0fd692cdd8ce8a09dbc669ccf49b56079
David <contato.davidalmeida@outlook.com>
2023-12-22 21:31:05 -0300

    ✅ Update: Firebase Configuration file

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,18 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { config } from "dotenv";
-
-config();
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { FormDataProps } from "../@types";
 
 const firebaseConfig = {
-	apiKey: process.env.FIRESTORE_API_KEY,
-	authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
-	projectId: process.env.FIRESTORE_PROJECT_ID,
-	storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
-	appId: process.env.FIRESTORE_APP_ID,
-	measurementId: process.env.FIRESTORE_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+	authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIRESTORE_APP_ID,
+	measurementId: process.env.REACT_APP_FIRESTORE_MEASUREMENT_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+
+export const insertData = async (data: FormDataProps) => {
+	const db = getFirestore(app);
+	const collectionDB = collection(db, "hellfire-club");
+	const docRef = await addDoc(collectionDB, data);
+
+	return docRef.id;
+};
