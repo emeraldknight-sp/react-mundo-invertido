f081abbd5f357b8ffb8b425d51135d88c6d9537a
David <contato.davidalmeida@outlook.com>
2023-12-22 21:30:08 -0300

    ✅ Update: Adding Toaster

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "./context";
 import { App } from "./App";
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<AppProvider>
+			<Toaster position="top-center" reverseOrder={false} />
 			<App />
 		</AppProvider>
 	</React.StrictMode>,
